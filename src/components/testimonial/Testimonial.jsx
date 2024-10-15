aeaf79fe08b4ab31bf7a030365e37cff2193bed3
KHIZERBUTT07 <153557362+KHIZERBUTT07@users.noreply.github.com>
2024-10-16 03:19:32 +0500

    ecommerceStoreDone

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -13,7 +13,7 @@ function Testimonial() {
                     <div className="flex flex-wrap -m-4">
                         <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                             <div className="h-full text-center">
-                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="public/images/khizerpic-removebg-preview.png" />
+                                <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://w7.pngwing.com/pngs/306/70/png-transparent-computer-icons-management-admin-silhouette-black-and-white-neck.png" />
                                 <p style={{color: mode === 'dark' ? 'white' : ''}} className="leading-relaxed">Best E-commerce Store with Best Products Quality & it is not less than any other Big E-commerce store.</p>
                                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                                 <h2 style={{color: mode === 'dark' ? '#ff4162' : ''}} className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Mr Khizer</h2>
